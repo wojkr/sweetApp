bc4c99c02b275ef65dea7d72c95c429215c65ea1
wojciech krupa <wojciech.w.krupa@gmail.com>
2022-12-06 13:05:27 +0000

    helmet config moved to separate file

--- app.js
+++ app.js
@@ -45,29 +45,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(mongoSanitize());
 
-const scriptSrcUrls = [
-  "https://stackpath.bootstrapcdn.com",
-  "https://api.tiles.mapbox.com",
-  "https://api.mapbox.com",
-  "https://kit.fontawesome.com",
-  "https://cdnjs.cloudflare.com",
-  "https://cdn.jsdelivr.net",
-];
-const styleSrcUrls = [
-  "https://cdn.jsdelivr.net",
-  "https://kit-free.fontawesome.com",
-  "https://stackpath.bootstrapcdn.com",
-  "https://api.mapbox.com",
-  "https://api.tiles.mapbox.com",
-  "https://fonts.googleapis.com",
-  "https://use.fontawesome.com",
-];
-const connectSrcUrls = [
-  "https://api.mapbox.com",
-  "https://*.tiles.mapbox.com",
-  "https://events.mapbox.com",
-];
-const fontSrcUrls = [];
+const { scriptSrcUrls, styleSrcUrls, connectSrcUrls, fontSrcUrls } = require('./utils/helmetConfigCSP')
+
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
---
+++ utils/helmetConfigCSP.js
@@ -0,0 +1,24 @@
+module.exports.scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com",
+    "https://api.tiles.mapbox.com",
+    "https://api.mapbox.com",
+    "https://kit.fontawesome.com",
+    "https://cdnjs.cloudflare.com",
+    "https://cdn.jsdelivr.net",
+];
+module.exports.styleSrcUrls = [
+    "https://cdn.jsdelivr.net",
+    "https://kit-free.fontawesome.com",
+    "https://stackpath.bootstrapcdn.com",
+    "https://api.mapbox.com",
+    "https://api.tiles.mapbox.com",
+    "https://fonts.googleapis.com",
+    "https://use.fontawesome.com",
+];
+module.exports.connectSrcUrls = [
+    "https://api.mapbox.com",
+    "https://*.tiles.mapbox.com",
+    "https://events.mapbox.com",
+];
+module.exports.fontSrcUrls = [];
+
