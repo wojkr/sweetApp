adc1b120d504041c3e2f396d4693817240c04d7d
wojciech krupa <wojciech.w.krupa@gmail.com>
2022-12-13 22:37:50 +0000

    deploy try 4

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const LocalStrategy = require("passport-local");
 
 const MongoStore = require('connect-mongo');
 
-const post = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 const secret = process.env.SECRET || "TheGemLettuceIsHidden";
 const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/sweetApp";
 // const dbUrl = "mongodb://localhost:27017/sweetApp";
